3c98493e62432831b4780077674e469f7cd5669c
NovaTech-com <saedsaroj@gmail.com>
2025-02-17 14:45:30 -0600

    aws_amplify

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I miss you Shakira Guadalupe, return plis HOLA MUNDO
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
